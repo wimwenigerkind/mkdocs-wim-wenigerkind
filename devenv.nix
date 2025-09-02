e8affdaa4da754b83ffdf6f1f545fb94366ee4de
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-09-02 14:40:35 +0200

    Add mkdocs plugins for git revision date and committers

--- devenv.nix
+++ devenv.nix
@@ -7,6 +7,8 @@
     python3Packages.mike
     python3Packages.mkdocs
     python3Packages.mkdocs-material
+    python3Packages.mkdocs-git-revision-date-localized-plugin
+    python3Packages.mkdocs-git-committers-plugin-2
     # Falls weitere Extensions gebraucht werden:
     python3Packages.pymdown-extensions
   ];
